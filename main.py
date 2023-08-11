b63e5ad8d1960ebec316fd5ac878d02ef428affb
mishpro <root@venusbiser.ru>
2023-08-11 10:49:59 +0700

    Upd

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import sleep
 
 api_id = 24851427
 api_hash = 'b73fb7bae6362ba5d37825d8334ec903'
-app = Client("open", api_id=api_id, api_hash=api_hash)
+app = Client("open", api_id=api_id, api_hash=api_hash, app_version="CaseOpen 1.0", device_model="@mishpro Scripts", lang_code="ru")
 print("\033[33mВведите номер телефона, затем английскую 'y', и код который придет из телеги. Если не напишет 'Готово', то нужно ввести пароль от тг\033[0m")
 try:
     app.start()
